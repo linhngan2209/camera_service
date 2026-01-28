580dcf5f8dbd8cd03f3c7031dd977cba43eced01
linhngan2209 <nganptlhe160415@fpt.edu.vn>
2026-01-29 03:02:42 +0700

    fix socket

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { IoAdapter } from '@nestjs/platform-socket.io';
+import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useWebSocketAdapter(new WsAdapter(app));
 
   app.setGlobalPrefix('api/v1');
   app.enableCors({
     origin: '*',
     methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type,Authorization',
-
   });
+
   await app.listen(8008);
-  app.useWebSocketAdapter(new IoAdapter(app));
-
 }
-
 bootstrap();
