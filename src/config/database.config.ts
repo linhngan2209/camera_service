e01e3a5bba9dbf7b5c47a694db8e187c0ace5e57
linhngan2209 <nganptlhe160415@fpt.edu.vn>
2026-01-28 19:00:03 +0700

    fix pass db

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -6,7 +6,7 @@ export const getDatabaseConfig = (configService: ConfigService): TypeOrmModuleOp
   host: configService.get<string>('DB_HOST', 'localhost'),
   port: configService.get<number>('DB_PORT', 3306),
   username: configService.get<string>('DB_USER', 'root'),
-  password: configService.get<string>('DB_PASSWORD', '0'),
+  password: configService.get<string>('DB_PASSWORD', 'Tan@26082000'),
   database: configService.get<string>('DB_NAME', 'camera_db'),
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true,
